528970e0fa5016be44da5e2b2edb625c59a64f44
Ralph <rdugue1@gmail.com>
2023-06-24 06:47:31 -0400

    Test application configuration

--- src/test/kotlin/com/ralphdugue/backend/arcadephito/ApplicationTest.kt
+++ src/test/kotlin/com/ralphdugue/backend/arcadephito/ApplicationTest.kt
@@ -6,6 +6,7 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.config.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,6 +15,9 @@ import kotlin.test.assertEquals
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        environment {
+            config = ApplicationConfig("application-test.conf")
+        }
         val client = createClient {
             install(ContentNegotiation) {
                 json()
